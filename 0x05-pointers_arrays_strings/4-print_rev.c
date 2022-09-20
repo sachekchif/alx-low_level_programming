0c33c1bdd17ca8c852c9c69673e182324c118c77
samuel <samuelchif1245@gmail.com>
2022-09-19 19:09:05 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len, index;
 
-	while (s[index++])
+	len = 0;
+	while (s[len] != '\0')
 		len++;
 
 	for (index = len - 1; index >= 0; index--)
+	{
 		_putchar(s[index]);
-
+	}
 	_putchar('\n');
 }
