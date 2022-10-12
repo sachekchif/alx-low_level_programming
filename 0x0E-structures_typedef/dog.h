ddf02f9b21f2688f06192417b9c04a7b11ff380d
samuel <samuelchif1245@gmail.com>
2022-10-11 17:55:42 -0700

    Mandatory

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,17 @@
 #define FILE_DOG
 
 /**
- *  * struct dog - structure dog
- *   * @name: atribute pointer to char name's dog
- *    * @age: atribute float age's dog
- *     * @owner: atribute pointer to char owner's dog
- *      */
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
 
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
